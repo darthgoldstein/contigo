0b26432241da0d28fe58434b82645fab70002d5a
Benjamin Goldstein <benjaming@d-fendsolutions.com>
2023-07-18 22:44:32 +0300

    feat: Add facebook config env variables

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -2,4 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const config = {};
+export const config = {
+    facebookClientToken: process.env.FACEBOOK_CLIENT_TOKEN ?? '',
+    facebookAppId: process.env.FACEBOOK_APP_ID ?? '',
+};
